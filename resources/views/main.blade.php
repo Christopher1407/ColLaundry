1b81f32b1f1e35e8edb48054b6ca36dd9e279b81
christopher1407 <christopher.20050@mhs.unesa.ac.id>
2022-06-07 05:16:55 +0700

    update

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,83 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Menghubungkan dengan view template master -->
+@extends('master')
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+<!-- isi bagian judul halaman -->
+<!-- cara penulisan isi section yang pendek -->
+@section('judul_halaman', 'Halaman Home')
 
-  <title>Collaundry</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="{{ asset('style/assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('style/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+<!-- isi bagian konten -->
+<!-- cara penulisan isi section yang panjang -->
+@section('konten')
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('style/assets/vendor/animate.css/animate.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('style/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('style/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('style/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('style/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('style/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-  <link href="{{ asset('style/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="{{ asset('style/assets/css/style.css') }}" rel="stylesheet">
-
-<body>
-
-  <!-- ======= Top Bar ======= -->
-  <div id="topbar" class="fixed-top d-flex align-items-center ">
-    <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
-      <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-envelope-fill"></i><a href="mailto:contact@example.com">info@example.com</a>
-        <i class="bi bi-phone-fill phone-icon"></i> 0822 7777 6666
-      </div>
-    </div>
-  </div>
-
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center ">
-    <div class="container d-flex align-items-center justify-content-between">
-
-      <a class="logo">
-          
-      </a>
-
-      <!-- .navbar -->
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">Tentang</a></li>
-          <li><a class="nav-link scrollto" href="price.blade.php">Service</a></li>
-          <li><a class="nav-link scrollto" href="#pricing">Harga</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-    </div>
-  </header><!-- End Header -->
 
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex justify-cntent-center align-items-center">
@@ -86,24 +18,24 @@
       <div class="carousel-item active">
         <div class="carousel-container">
           <h2 class="animate__animated animate__fadeInDown">Selamat datang di <span>Collaundry</span></h2>
-          <p class="animate__animated animate__fadeInUp">Merupakan website penyedia jasa laundry untuk area Surabaya yang dikhususkan untuk Mahasiswa di sekitar kota Surabaya, namun juga melayani non-Mahasiswa</p>
+          <p class="animate__animated animate__fadeInUp">Merupakan website penyedia jasa laundry untuk area Surabaya yang mengedepankan untuk Mahasiswa di sekitar kota Surabaya, namun juga melayani non-Mahasiswa</p>
           <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
         </div>
       </div>
       <!-- Slide 2 -->
       <div class="carousel-item">
         <div class="carousel-container">
-          <h2 class="animate__animated animate__fadeInDown">Slide 2</h2>
-          <p class="animate__animated animate__fadeInUp">Slide 2</p>
-          <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+          <h2 class="animate_animated animate_fadeInDown">Selamat datang di <span>Collaundry</span></h2>
+          <p class="animate_animated animate_fadeInUp">Kami disini menerima untuk pengambilan cucian ke rumah pemesan, ataupun pemesan secara langsung datang ke tempat kami</p>
+          <a href="#about" class="btn-get-started animate_animated animate_fadeInUp scrollto">Read More</a>
         </div>
       </div>
       <!-- Slide 3 -->
       <div class="carousel-item">
         <div class="carousel-container">
-          <h2 class="animate__animated animate__fadeInDown">Slide 3</h2>
-          <p class="animate__animated animate__fadeInUp">Slide 3</p>
-          <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+          <h2 class="animate_animated animate_fadeInDown">Selamat datang di <span>Collaundry</span></h2>
+          <p class="animate_animated animate_fadeInUp">Moto kami yaitu "Kenyamanan dan Kepuasan kalian adalah penyemangat kami"</p>
+          <a href="#about" class="btn-get-started animate_animated animate_fadeInUp scrollto">Read More</a>
         </div>
       </div>
 
@@ -128,7 +60,7 @@
             <div class="icon-box">
               <div class="icon"><i class='bx bx-phone-call'></i></i></div>
               <h4 class="title"><a href="">Panggilan Cepat</a></h4>
-              <p class="description">Panggilan</p>
+              <p class="description">Dengan Panggilan dan respon yang cepat dalam melayani pelanggan demi membuat senang para pelanggan</p>
             </div>
           </div>
 
@@ -136,7 +68,7 @@
             <div class="icon-box">
               <div class="icon"><i class='bx bxs-wallet'></i></i></div>
               <h4 class="title"><a href="">Hemat Uang</a></h4>
-              <p class="description">Hemat</p>
+              <p class="description">Dengan harga yang ramah untuk anak Kos ditambah seringnya promo</p>
             </div>
           </div>
 
@@ -144,7 +76,7 @@
             <div class="icon-box">
               <div class="icon"><i class='bx bxs-t-shirt'></i></div>
               <h4 class="title"><a href="">Hasil Terbaik</a></h4>
-              <p class="description">Hasil</p>
+              <p class="description">Hasil yang memuaskan dengan kualitas atas membuat pakaian tetap terlihat seperti baru</p>
             </div>
           </div>
 
@@ -152,7 +84,7 @@
             <div class="icon-box">
               <div class="icon"><i class='bx bxs-time'></i></div>
               <h4 class="title"><a href="">Tepat Waktu</a></h4>
-              <p class="description">Waktu</p>
+              <p class="description">Singkatnya pengerjaan membuat Konsumen akan lebih senang dan tenang tanpa menunggu waktu yang tidak pasti</p>
             </div>
           </div>
         </div>
@@ -165,23 +97,23 @@
 
         <div class="section-title">
           <h2>TENTANG KAMI</h2>
-          <p>.................................................................</p>
+          <p>Collaundry merupakan pelayanan Laundry yang menawarkan jasa laundry dan mengedepankan untuk para Mahasiswa sekitar area kota Surabaya</p>
         </div>
 
         <div class="row content">
           <div class="col-lg-6">
             <p>
-              .......................................
+              Hal yang membuat Kami menjadi jasa Laundry yang tepercaya
             </p>
             <ul>
-              <li><i class="ri-check-double-line"></i>......................................</li>
-              <li><i class="ri-check-double-line"></i>......................................</li>
-              <li><i class="ri-check-double-line"></i>......................................</li>
+              <li><i class="ri-check-double-line"></i>Pelayanan yang cepat</li>
+              <li><i class="ri-check-double-line"></i>Hasil yang memuaskan</li>
+              <li><i class="ri-check-double-line"></i>Harga yang sangat ramah dikantong</li>
             </ul>
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
             <p>
-              ......................................
+              Untuk mengetahui tentang kami lebih lanjut dapat mengklik tombol dibawah ini
             </p>
             <a href="#" class="btn-learn-more">Pelajari Lebih lanjut</a>
           </div>
@@ -189,17 +121,17 @@
 
       </div>
     </section><!-- End About Us Section -->
-    
+
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
         <div class="container" data-aos="fade-up">
-  
-          <div class="section-title"> 
+
+          <div class="section-title">
             <h2>Contact Us</h2>
           </div>
-  
+
           <div class="row mt-1 d-flex justify-content-end" data-aos="fade-right" data-aos-delay="100">
-  
+
             <div class="col-lg-5">
               <div class="info">
                 <div class="address">
@@ -210,7 +142,7 @@
                 <div class="email">
                   <i class="bi bi-envelope"></i>
                   <h4>Email:</h4>
-                  <p>info@example.com</p>
+                  <p>info@Collaundry.com</p>
                 </div>
                 <div class="phone">
                   <i class="bi bi-phone"></i>
@@ -220,34 +152,42 @@
               </div>
             </div>
             <div class="col-lg-6 mt-5 mt-lg-0" data-aos="fade-left" data-aos-delay="100">
-              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                @if ($message = Session::get('error'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
+              <form action="{{ route('add_pesan') }}" method="POST">
+                  @csrf
                 <div class="row">
                   <div class="col-md-6 form-group">
-                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Nama" required>
                   </div>
                   <div class="col-md-6 form-group mt-3 mt-md-0">
-                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
                   </div>
                 </div>
                 <div class="form-group mt-3">
-                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                  <input type="text" class="form-control" name="judul" id="judul" placeholder="Judul" required>
                 </div>
                 <div class="form-group mt-3">
-                  <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                  <textarea class="form-control" name="pesan" id="pesan" rows="5" placeholder="Pesan" required></textarea>
                 </div>
-                <div class="my-3">
-                  <div class="loading">Loading</div>
-                  <div class="error-message"></div>
-                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                <div class="text-center">
+                  <button type="submit" class="btn btn-success">Kirim</button>
                 </div>
-                <div class="text-center"><button type="submit">Send Message</button></div>
               </form>
             </div>
           </div>
         </div>
       </section><!-- End Contact Section -->
     </main><!-- End #main -->
-  
+
 <!-- ======= Footer ======= -->
 <footer id="footer">
     <div class="footer-newsletter">
@@ -266,64 +206,4 @@
       </div>
     </div>
 
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-4 col-md-6 footer-contact">
-            <h4>Contact Us</h4>
-            <p>
-              Surabaya<br><br>
-              <strong>Phone:</strong> 0822 7777 6666<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
-          </div>
-          <div class="col-lg-4 col-md-6 footer-info">
-            <h3>Tentang Collaundry</h3>
-            <p>Jasa Laundry yang diutamakan untuk Mahasiswa daerah Surabaya dan sekitarnya dengan pelayanan yang cepat</p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="whatsapp   "><i class="bx bxl-whatsapp"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Collaundry</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        Designed by <a href="#">Kelompok 6</a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('style/assets/vendor/aos/aos.js') }}"></script>
-  <script src="{{ asset('style/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('style/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-  <script src="{{ asset('style/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="{{ asset('style/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-  <script src="{{ asset('style/assets/vendor/php-email-form/validate.js') }}"></script>
-
-  <!-- Template Main JS File -->
-  <script src="{{ asset('style/assets/js/main.js') }}"></script>
-
- </body>
-</html>
+@endsection
